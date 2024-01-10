2b07084a5ace18a36eea9eee6c9fca5e6468d728
Sonic-YuJianxiang <1347256906@qq.com>
2024-01-11 00:47:00 +0200

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import { Jwt } from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -12,6 +13,32 @@ export const signup = async (req, res, next) => {
         res.status(201).json({ message: "User created successfully" });
     } catch (error) {
         // next(errorHandler(550, "error from the function"));
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(
+            password,
+            existingUser.password
+        );
+        if (!validPassword) next(errorHandler(401, "Wrong Credentials!"));
+        // create a token
+        const token = Jwt.sign(
+            { id: existingUser._id },
+            process.env.JWT_SECRET,
+        );
+        const { password: pwd, ...rest } = existingUser._doc;
+
+        // put the token into a cookie and send the cookie in the response
+        res.cookie("access_token", token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+    } catch (error) {
         next(error);
     }
 };
