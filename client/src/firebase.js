aa900369d9cccc2f4f42a813df3db9d295229d35
Sonic-YuJianxiang <1347256906@qq.com>
2024-01-27 00:42:39 +0800

    complete profile page UI

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "house-estate-ca646.firebaseapp.com",
   projectId: "house-estate-ca646",
   storageBucket: "house-estate-ca646.appspot.com",
