c80bbad6103cc7fb9778a6eea91a9286e512d0c7
Sonic-YuJianxiang <1347256906@qq.com>
2024-03-30 22:12:02 +0200

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from '../models/listing.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createListing = async (req, res) => {
     try {
@@ -7,9 +8,25 @@ export const createListing = async (req, res) => {
             listing,
         });
     } catch (error) {
-        return res.status(500).json({
-            success: false,
-            error: error.message,
-        });
+        next(error);
+    }
+};
+
+export const deleteListing = async (req, res) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found'));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You are not authorized to delete this listing'));
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json("Listing has been deleted successfully!");
+    } catch (error) {
+        next(error);
     }
 };
