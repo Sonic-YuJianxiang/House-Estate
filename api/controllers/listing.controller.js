82bc0435ef30752ba7decf9dd529a71d138f93d4
Sonic-YuJianxiang <1347256906@qq.com>
2024-03-30 22:54:08 +0200

    create update listing api router

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,7 +1,7 @@
 import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
 
-export const createListing = async (req, res) => {
+export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body);
         return res.status(201).json({
@@ -12,7 +12,7 @@ export const createListing = async (req, res) => {
     }
 };
 
-export const deleteListing = async (req, res) => {
+export const deleteListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
 
     if (!listing) {
@@ -26,6 +26,28 @@ export const deleteListing = async (req, res) => {
     try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json("Listing has been deleted successfully!");
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found'));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You are not authorized to update this listing'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
     } catch (error) {
         next(error);
     }
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -30,7 +30,7 @@ export const updateUser = async (req, res, next) => {
     }
 };
 
-export const deleteUser = async (req, res) => {
+export const deleteUser = async (req, res, next) => {
     if (req.user.id == req.params.id) return next(errorHandler(401, 'You can only delete your own account'));
     try {
         await User.findByIdAndDelete(req.params.id);
@@ -41,13 +41,13 @@ export const deleteUser = async (req, res) => {
     }
 };
 
-export const getUserListings = async (req, res) => {
+export const getUserListings = async (req, res, next) => {
     if (req.user.id !== req.params.id){
         try {
             const listings = await Listing.find({ userRef: req.params.id });
             res.status(200).json(listings);
         } catch (error) {
-            
+            next(error);
         }
     }else{
         return next(errorHandler(401, 'You can only view your own listings'));
